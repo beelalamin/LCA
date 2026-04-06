ecd2352d4607a0fa291942bb5afe42e019b5a1e2
ghanimlawfirm <ghanimlawfirm.qa@gmail.com>
2026-04-06 18:26:49 +0300

    fix: Fixed broken layout by Switching to renderHook for custom CSS

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,7 +38,10 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->font('system-ui')
-            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->renderHook(
+                PanelsRenderHook::HEAD_END,
+                fn (): string => Blade::render("@vite('resources/css/filament/admin/theme.css')"),
+            )
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
